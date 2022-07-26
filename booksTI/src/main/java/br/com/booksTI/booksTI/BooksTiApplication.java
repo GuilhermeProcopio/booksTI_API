f053ad27e2ba50e767ad02e2147513a910793950
Guilherme <guilhermecprocopio@gmail.com>
2022-07-26 17:33:15 -0300

    Initial project build

---
+++ booksTI/src/main/java/br/com/booksTI/booksTI/BooksTiApplication.java
@@ -0,0 +1,13 @@
+package br.com.booksTI.booksTI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksTiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksTiApplication.class, args);
+	}
+
+}
---
+++ booksTI/src/test/java/br/com/booksTI/booksTI/BooksTiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.booksTI.booksTI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksTiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
